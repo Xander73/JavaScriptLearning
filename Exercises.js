6b8bfb2caa85fdac29955524a6492e0052cfd679
Alex <sanjok@bk.ru>
2021-12-24 22:55:12 +0300

    Lesson 3

--- Exercises.js
+++ Exercises.js
@@ -1,140 +1,122 @@
+//ДЗ к 3-му уроку
+
 'use strict'
 
-//пример 1
-
-let a = 1, b = 1, c, d;
-
-/*
-1. выполняется префиксный инкремент а получаем а = 2
-2. с присваивается значение 2
-*/
-
-c = ++a;
-
-alert(c); // ответ: 2
-
-//пример 2
-
-/*
-1. в присваивается значение b = 1
-2. выполняется постфиксный инкремент b получаем b = 2
-*/
-d = b++;
-
-alert(d); //ответ: 1
-
-//пример 3
-
-/*
-1. выполняется префиксный инкремент а получаем а = 3
-2. выполняется сложение 2 и а получаем 5
-3. присваиваем в с значение 5
-*/
-c = 2 + ++a;
-
-alert(c); //ответ: 5
-
-//пример 4
-
-/*
-1. выполняется сложение 2 и b получаем 4 
-2. выполняется постфиксный инкремент b получаем b = 3
-3. присваиваем в d значение 4
-*/
-d = 2 + b++;
-
-alert(d); //ответ: 4
-
-alert(a); //3
-
-alert(b); //3
-
-//Упражнение 2 
-
-a = 2;
-
-/*
-1. выполняется умножение а на 2 получаем 4
-2. выполняется сложение 1 и 4 получаем 5
-3. выполняется присваивание x значение 5
-*/
-let x = 1 + (a *= 2);
-
-console.log(x);
-
-//Упражнение 3
-a = 3;
-b = 2;
-
-if (a > 0 && b > 0) {
-    console.log(a - b);
-}
-else if (a < 0 && b < 0) {
-    console.log(a * b);
-}
-else if ((a * b) < 0) {
-    console.log(a + b);
-}
-
-//Упражнение 4
-function Add(num1, num2) {
-    return num1 + num2;
-}
-
-function Sub(num1, num2) {
-    return num1 - num2;
-}
-
-function Mult(num1, num2) {
-    return num1 * num2;
-}
-
-function Div(num1, num2) {
-    if (num2 !== 0) {
-        return num1 / num2;
-    }
-    else {
-        alert('На 0 делить нельзя')
+//1-е задание
+for (let i = 0; i <= 10; ++i) {
+    if (i == 0) {
+        console.log(i + " - это ноль");
+    } else if (i % 2 == 0) {
+        console.log(i + " - четное число");
+    } else if (i % 2 != 0) {
+        console.log(i + " - нечетное число");
     }
 }
+console.log('\n');
 
-//Упражнение 5
+//2-е задание
+const post = {
+    author: "John", //вывести этот текст
+    postId: 23,
+    comments: [
+        {
+            userId: 10,
+            userName: "Alex",
+            text: "lorem ipsum",
+            rating: {
+                likes: 10,
+                dislikes: 2 //вывести это число
+            }
+        },
+        {
+            userId: 5, //вывести это число
+            userName: "Jane",
+            text: "lorem ipsum 2", //вывести этот текст
+            rating: {
+                likes: 3,
+                dislikes: 1
+            }
+        },
+    ]
+};
 
-function mathOperation(arg1, arg2, operation) {
-    switch (operation.toLowerCase()) {
-        case "add": {
-            console.log(Add(arg1, arg2));
-            break;
-        }
-        case "sub": {
-            console.log(Sub(arg1, arg2));
-            break;
-        }
-        case "div": {
-            console.log(Div(arg1, arg2));
-            break;
-        }
-        case "mult": {
-            console.log(Mult(arg1, arg2));
-            break;
-        }
-        default: {
-            alert("Действия '" + operation + "' нет")
-        }
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
+console.log('\n');
+
+//3-е задание
+const products1 = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+products1.forEach(item => item.price *= 0.85)
+
+console.log("\n");
+console.log("Объекты со сниженной ценой");
+console.log(products1);
+
+//4-е упражнение
+const products2 = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ]
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: []
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg"
+        ]
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
+
+let productsWithPhotos = products2.filter((item) => {
+    if ("photos" in item) {
+        return item.photos.length > 0;
     }
+})
+console.log("\n");
+console.log("Объекты с фотографиями");
+console.log(productsWithPhotos);
+
+products2.sort((item1, item2) => item1.price - item2.price);
+
+console.log("\n");
+console.log("Сортировка по цене (от низкой к высокой)");
+console.log(products2);
+
+//6-е упражнение
+let star = "";
+
+console.log("\n");
+console.log("Горка");
+for (let i = 0; i < 20; ++i) {
+    star += "*";
+    console.log(star);
 }
-console.log("Add");
-mathOperation(2, 3, "Add");
-
-console.log("Sub");
-mathOperation(2, 3, "Sub");
-
-console.log("Div");
-mathOperation(2, 3, "Div");
-
-console.log("Mult");
-mathOperation(2, 3, "Mult");
-
-console.log("Plus");
-mathOperation(2, 3, "Plus");
-
